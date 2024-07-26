a7cecb2184b820d03140ede97c3d2ea2eb199534
TanishaNakade <142479909+TanishaNakade@users.noreply.github.com>
2024-07-26 19:59:41 +0530

    Add files via upload

---
+++ MethodOverriding.java
@@ -0,0 +1,11 @@
+/**
+ * MethodOverriding
+ */
+public class MethodOverriding {
+
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "2";
+        System.out.println(a+b);
+    }
+}
